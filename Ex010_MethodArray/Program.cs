ddebfe49451dd7628273b6ae8d2afbe493ecb269
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-04 22:37:27 +0300

    Ex010_MethodArray

---
+++ Ex010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 11, 21, 31, 19, 15, 61, 17, 18, 19 };
+int n = array.Length;
+int find = 19;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        System.Console.WriteLine(index);
+        break; //стоп слово, остановит на первом найденном
+    }
+    index++;
+}

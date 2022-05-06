ab1f080988a61b5e42127b343e01c5dbc5e8e161
DmitriyKlaes <89119584059@mail.ru>
2022-05-06 16:25:10 +0300

    Ex008-1_FindMaxFunction

--- Ex008-1_FIndMaxFunction/Program.cs
+++ Ex008-1_FIndMaxFunction/Program.cs
@@ -7,46 +7,36 @@ int Max(int arg1, int arg2, int arg3)
     if (arg3 > result) result = arg3;
     return result;
 }
-
-Console.WriteLine("Выбрать случайные числа от 1 до 100 (введите Y или N)?:");
-string choice = Console.ReadLine();
-int[] array = new int[9];
-if (choice == "y")
+Console.WriteLine("Выбрать случайные 9 чисел от 1 до 100 (введите Y или N)?:");
+string choice = Console.ReadLine()!;
+while (choice.ToLower() == "y")
 {
+    int[] array = new int[9];
     for (int i = 0; i < array.Length; i++)
     {
-        System.Console.WriteLine("что то не так");
         Random rand = new Random();
         array[i] = rand.Next(1, 100);
     }
-}
-/*if (choice.ToLower() == "n")
-{
-    Console.WriteLine("Введите 9 случайных чисел через (Пробел):");
-}*/
-else
-{
-    System.Console.WriteLine("Неверный выбор!");
-}
-string[] arrayNumber = (Console.ReadLine().Split(' '));
-for (int i = 0; i < arrayNumber.Length; i++)
-{
-    array[i] = int.Parse(arrayNumber[i]);
-}
-int max = Max(Max(array[0], array[1], array[2]),
+    int max = Max(Max(array[0], array[1], array[2]),
               Max(array[3], array[4], array[5]),
               Max(array[6], array[7], array[8]));
-string str = string.Join(", ", arrayNumber);
-Console.WriteLine($"Максимальным из чисел ({str}) является {max}");
-
-
-/*
-int[] array = new int[9];
-for (int i = 0; i < array.Length; i++)
-{
-    Random rand = new Random();
-    array[i] = rand.Next(1, 100);
+    string str = string.Join(", ", array);
+    Console.WriteLine($"Максимальным из чисел ({str}) является {max}");
+    break;
 }
-string str = string.Join(", ", array);
-System.Console.WriteLine(str);
-*/
+while (choice.ToLower() == "n")
+{
+    Console.WriteLine("Введите 9 случайных чисел через (Пробел):");
+    string[] arrayNumber = (Console.ReadLine()!.Split(' '));
+    int[] array = new int[9];
+    for (int i = 0; i < arrayNumber.Length; i++)
+    {
+        array[i] = int.Parse(arrayNumber[i]);
+    }
+    int max = Max(Max(array[0], array[1], array[2]),
+              Max(array[3], array[4], array[5]),
+              Max(array[6], array[7], array[8]));
+    string str = string.Join(", ", arrayNumber);
+    Console.WriteLine($"Максимальным из чисел ({str}) является {max}");
+    break;
+}

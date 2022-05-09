ed4aa92a17f4bbd31f5829b87a67eab7b92cb94d
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-09 23:47:44 +0300

    апгрейд Ex008-1 - вынес переменную max за пределы тел цикла

--- Ex008-1_FIndMaxFunction/Program.cs
+++ Ex008-1_FIndMaxFunction/Program.cs
@@ -9,34 +9,28 @@ int Max(int arg1, int arg2, int arg3)
 }
 Console.WriteLine("Выбрать случайные 9 чисел от 1 до 100 (введите Y или N)?:");
 string choice = Console.ReadLine()!;
+int[] array = new int[9];
 while (choice.ToLower() == "y")
 {
-    int[] array = new int[9];
     for (int i = 0; i < array.Length; i++)
     {
         Random rand = new Random();
         array[i] = rand.Next(1, 100);
     }
-    int max = Max(Max(array[0], array[1], array[2]),
-              Max(array[3], array[4], array[5]),
-              Max(array[6], array[7], array[8]));
-    string str = string.Join(", ", array);
-    Console.WriteLine($"Максимальным из чисел ({str}) является {max}");
     break;
 }
 while (choice.ToLower() == "n")
 {
     Console.WriteLine("Введите 9 случайных чисел через (Пробел):");
     string[] arrayNumber = (Console.ReadLine()!.Split(' '));
-    int[] array = new int[9];
     for (int i = 0; i < arrayNumber.Length; i++)
     {
         array[i] = int.Parse(arrayNumber[i]);
     }
-    int max = Max(Max(array[0], array[1], array[2]),
-              Max(array[3], array[4], array[5]),
-              Max(array[6], array[7], array[8]));
-    string str = string.Join(", ", arrayNumber);
-    Console.WriteLine($"Максимальным из чисел ({str}) является {max}");
     break;
 }
+int max = Max(Max(array[0], array[1], array[2]),
+              Max(array[3], array[4], array[5]),
+              Max(array[6], array[7], array[8]));
+    string str = string.Join(", ", array);
+    Console.WriteLine($"Максимальным из чисел ({str}) является {max}");

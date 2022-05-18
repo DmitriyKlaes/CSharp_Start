3a65f3dc242a850943d6ad23f4aea68f129198ae
Dmitriy_Klaes <89119584059@mail.ru>
2022-05-19 00:11:47 +0300

    добавид exes 012/-1/-2/-3 from lection 22-05-17

---
+++ Ex012_MethodsList/Program.cs
@@ -0,0 +1,60 @@
+﻿// Вид 1
+void Method()
+{
+    System.Console.WriteLine("Автор Димуля");
+}
+Method();
+
+// Вид2
+void Method2(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        System.Console.WriteLine(msg);
+        i++;
+    }
+    System.Console.WriteLine(msg);
+}
+Method2(msg: "Текст", count: 4);
+Method2(count: 4, msg: "Новый текст");
+
+// Вид3
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+int year = Method3();
+System.Console.WriteLine(year);
+
+// Вид4
+string Method4(int count, string c)
+{
+    int i = 0;
+    string result = String.Empty; // или "" вместо String.Empty
+
+    while (i < count)
+    {
+        result = result + c;
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "asdf");
+System.Console.WriteLine(res);
+
+// Вид4(1)
+
+string Method4(int count, string text)
+{
+    string result = String.Empty; // или "" вместо String.Empty
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+System.Console.WriteLine(res);

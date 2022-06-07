40d4842d72ecbf763b7445fca1608db0cbae0e59
Dmitriy_Klaes <89119584059@mail.ru>
2022-06-07 22:59:49 +0300

    add Exes 015/016/017/018/016 from lection 22-06-07

---
+++ Ex015-NumberFromTo/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Собираем строку от а до б.
+*/
+
+string NumbersRec(int a, int b)
+{
+    //if (a <= b)
+    //{
+    //    return NumbersRec(a + 1, b) + $"{a} ";
+    //}
+    if (a <= b)
+    {
+        return $"{a} " + NumbersRec(a + 1, b);
+    }
+    else
+    {
+        return String.Empty;
+    }
+}
+
+Console.WriteLine(NumbersRec(1, 10));
